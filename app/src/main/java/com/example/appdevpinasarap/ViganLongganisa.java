f58c9da4f9c556c7e3765f49c80dd70644f3573c
Genevieve V. Carranza <gvcarranza2020@plm.edu.ph>
2023-01-06 15:35:38 +0800

    Vigan Longganisa Page

---
+++ app/src/main/java/com/example/appdevpinasarap/ViganLongganisa.java
@@ -0,0 +1,14 @@
+package com.example.appdevpinasarap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViganLongganisa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vigan_longganisa);
+    }
+}
