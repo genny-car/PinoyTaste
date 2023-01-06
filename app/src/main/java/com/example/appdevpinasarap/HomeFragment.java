2aecaedc5af5d70da658861b37f62c21af65d487
mocpagsuyuin2020 <78241250+mocpagsuyuin2020@users.noreply.github.com>
2023-01-06 17:59:43 +0800

    home greeting ver 2

--- app/src/main/java/com/example/appdevpinasarap/HomeFragment.java
+++ app/src/main/java/com/example/appdevpinasarap/HomeFragment.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
@@ -66,7 +67,12 @@ public class HomeFragment extends Fragment {
                     txtviewhomename.setText(name);
                 }
                 }
-            });
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         return v;
     }
 }
