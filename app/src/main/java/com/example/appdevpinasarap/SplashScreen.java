50b81964e1a05fb253d3147837ab93be9112b164
mocpagsuyuin2020 <78241250+mocpagsuyuin2020@users.noreply.github.com>
2023-01-08 17:51:12 +0800

    pinasarap logo icon

--- app/src/main/java/com/example/appdevpinasarap/SplashScreen.java
+++ app/src/main/java/com/example/appdevpinasarap/SplashScreen.java
@@ -15,7 +15,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
