76ab8ec3c9847d83ef0a50e9a4198edd7309b961
mocpagsuyuin2020 <78241250+mocpagsuyuin2020@users.noreply.github.com>
2023-01-08 14:59:34 +0800

    text fix (vigan longganisa)

--- app/src/main/java/com/example/appdevpinasarap/RegionI.java
+++ app/src/main/java/com/example/appdevpinasarap/RegionI.java
@@ -3,12 +3,14 @@ package com.example.appdevpinasarap;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegionI extends AppCompatActivity {
     ImageButton backbtn_reg1;
+    Button btnilocossur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +18,19 @@ public class RegionI extends AppCompatActivity {
         setContentView(R.layout.activity_region_i);
 
         backbtn_reg1 = (ImageButton) findViewById(R.id.backbtn_reg1);
+        btnilocossur = (Button) findViewById(R.id.btnilocossur);
 
         backbtn_reg1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(RegionI.this,Luzon.class));
+            }
+        });
+
+        btnilocossur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegionI.this,ViganLongganisa.class));
             }
         });
 
--- app/src/main/java/com/example/appdevpinasarap/ViganLongganisa.java
+++ app/src/main/java/com/example/appdevpinasarap/ViganLongganisa.java
@@ -2,13 +2,54 @@ package com.example.appdevpinasarap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ViganLongganisa extends AppCompatActivity {
+
+    TextView txtlongganisa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vigan_longganisa);
+
+        txtlongganisa = (TextView) findViewById(R.id.txtlongganisa);
+
+        setHtmlTextView(txtlongganisa,"<b>Prep Time</b>\n" +
+                "<br>    15 mins\n</br>" +
+                "<br>     Cooking Time\n</br>" +
+                "<br>     7 mins\n</br>" +
+                "<br>     Total Time\n</br>" +
+                "<br>     17 mins\n</br>" +
+                "<br>\n</br>" +
+                "<br> <b>Ingredients:</b>\n</br>" +
+                "<br>     3/4 kilogram ground pork\n</br>" +
+                "<br>     1/4 kilogram pork fat\n</br>" +
+                "<br>     1/4 cup garlic, minced\n</br>" +
+                "<br>     1 teaspoon ground black pepper\n</br>" +
+                "<br>     1/4 cup soy sauce\n</br>" +
+                "<br>     1 teaspoon annatto powder (atsuete)\n</br>" +
+                "<br>     1/4 cup iloko vinegar (sukang iloko), or vinegar\n</br>" +
+                "<br> \n</br>" +
+                "<br> <b>How to Cook</b>\n</br>" +
+                "<br>    ~ In a large bowl, mix ground pork, pork fat, garlic, pepper, soy sauce, atsuete powder, and vinegar until well blended.\n</br>" +
+                "<br>    ~ Portion meat mixture using an ice cream scoop onto a sheet of wax paper, measuring 5'' x 5''-inches. Roll until it forms into a log. Repeat with remaining meat mixtures\n</br>" +
+                "<br>    ~ Chill for at least 12 hours.\n</br>" +
+                "<br> \n</br>" +
+                "<br> <i><b>Originated</b>\n</br>" +
+                "<br>     Luzon\n</br>" +
+                "<br>     Region I, Ilocos Norte</i></br>");
+    }
+    public void setHtmlTextView(TextView txtlongganisa, String html) {
+        if (Build.VERSION.SDK_INT >= 24) {
+            txtlongganisa.setText(Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY));
+        } else {
+            txtlongganisa.setText(Html.fromHtml(html));
+        }
     }
 }
