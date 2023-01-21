8b28d34aa3e6e5b1d343aeeca61c6201d7b0a9de
mocpagsuyuin2020 <78241250+mocpagsuyuin2020@users.noreply.github.com>
2023-01-21 13:56:15 +0800

    change password fix

--- app/src/main/java/com/example/appdevpinasarap/Change_password.java
+++ app/src/main/java/com/example/appdevpinasarap/Change_password.java
@@ -12,10 +12,16 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class Change_password extends AppCompatActivity {
 
@@ -26,6 +32,7 @@ public class Change_password extends AppCompatActivity {
     String UserID;
     FirebaseAuth zAuth;
     FirebaseUser user;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,6 +85,7 @@ public void changePass() {
                 txtnewpass.setText("");
                 txtconpass.setText("");
                 Toast.makeText(Change_password.this, "Password Changed Successfully", Toast.LENGTH_SHORT).show();
+                update(txtnewpassword);
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -85,6 +93,21 @@ public void changePass() {
                 Toast.makeText(Change_password.this, "Error has Occurred", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+    private void update(String txtnewpassword){
+        HashMap nUpdate =new HashMap();
+
+        nUpdate.put("password",txtnewpassword);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("profdata");
+        reference.child(user.getUid()).updateChildren(nUpdate).addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if (task.isSuccessful()){
+                }
+                }
+            });
     }
 }
 
